4db9de0b40be46f96846b5fe14bdea293ae36c07
kpilszak <klaudia.pilszak@gmail.com>
2024-12-31 07:52:13 -0500

    Style navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,14 @@
 import whiteLogo from '../images/tinder_logo_white.png'
 import colorLogo from '../images/color-logo-tinder.png'
 
-const Nav = ({ minimal }) => {
+const Nav = ({ minimal, authToken }) => {
     return (
         <nav>
             <div className="logo-container">
                 <img className="logo" src={minimal ? colorLogo : whiteLogo} />
             </div>
+
+            {!authToken && <button className="nav-button">Log in</button>}
         </nav>
     )
 }
