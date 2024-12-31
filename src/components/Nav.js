c110d960e45b6e5badbdf9161f6390887f3c86c1
kpilszak <klaudia.pilszak@gmail.com>
2024-12-31 07:40:04 -0500

    Add logo

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,6 +1,13 @@
-const Nav = () => {
+import whiteLogo from '../images/tinder_logo_white.png'
+import colorLogo from '../images/color-logo-tinder.png'
+
+const Nav = ({ minimal }) => {
     return (
-        <div>Nav</div>
+        <nav>
+            <div className="logo-container">
+                <img className="logo" src={minimal ? colorLogo : whiteLogo} />
+            </div>
+        </nav>
     )
 }
 export default Nav
