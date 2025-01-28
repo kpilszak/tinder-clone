3ffbd72e1d0e99aed3c65036b634938a0b6ff69f
kpilszak <klaudia.pilszak@gmail.com>
2025-01-28 15:06:09 -0500

    Return users

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 const PORT = 8000
 const express = require('express')
 const { MongoClient } = require('mongodb')
-const uri = 'mongodb+srv://kpilszak:<db_password>@cluster0.lla0p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+const uri = 'mongodb+srv://kpilszak:<your_password>@cluster0.lla0p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 const app = express()
 
@@ -11,6 +11,21 @@ app.get('/', (req, res) => {
 
 app.get('/signup', (req, res) => {
     res.json('Hello to my app')
+})
+
+app.get('/users', async (req, res) => {
+    const client = new MongoClient(uri)
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const returnedUsers = await users.find().toArray()
+        res.send(returnedUsers)
+    } finally {
+        await client.close() 
+    }
 })
 
 app.listen(PORT, () => console.log('Server running on PORT ' + PORT))
