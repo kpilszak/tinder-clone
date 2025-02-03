6d73341843df359ab570bcc28d68933e17bb8c7a
kpilszak <klaudia.pilszak@gmail.com>
2025-02-03 15:48:12 -0500

    Add signup functionality

--- server/index.js
+++ server/index.js
@@ -1,16 +1,56 @@
 const PORT = 8000
 const express = require('express')
 const { MongoClient } = require('mongodb')
+const { v1: uuidv4 } = require('uuid')
+const jwt = require('jsonwebtoken')
+const cors = require('cors')
+const bcrypt = require('bcrypt')
+
 const uri = 'mongodb+srv://kpilszak:@cluster0.lla0p.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
 
 const app = express()
+app.use(cors())
+app.use(express.json())
 
 app.get('/', (req, res) => {
     res.json('Hello to my app')
 })
 
-app.get('/signup', (req, res) => {
+app.post('/signup', async (req, res) => {
     const client = new MongoClient(uri)
+    const { email, password } = req.body
+    const generatedUserId = uuidv4()
+    const hashedPassword = await bcrypt.hash(password, 10)
+
+    try {
+        await client.connect()
+        const database = client.db('app-data')
+        const users = database.collection('users')
+
+        const existingUser = await users.findOne({ email })
+
+        if (existingUser) {
+            return res.status(409).send('User already exists. Please login')
+        }
+
+        const sanitizedEmail = email.toLowerCase()
+
+        const data = {
+            user_id: generatedUserId,
+            email: sanitizedEmail,
+            hashed_password: hashedPassword
+        }
+        const insertedUser = await users.insertOne(data)
+
+        const token = jwt.sign(insertedUser, sanitizedEmail, {
+            expiresIn: 60 * 24,
+        })
+
+        res.status(201).json({ token, userId: generatedUserId, email: sanitizedEmail })
+
+    } finally {
+        await client.close()
+    }
 })
 
 app.get('/users', async (req, res) => {
