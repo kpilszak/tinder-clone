19f25605f624ebb49f03d040aed8827fa59b82b2
kpilszak <klaudia.pilszak@gmail.com>
2025-02-11 08:51:39 -0500

    Add get user by id endpoint

--- src/components/ChatContainer.js
+++ src/components/ChatContainer.js
@@ -2,9 +2,9 @@ import ChatHeader from './ChatHeader'
 import MatchesDisplay from './MatchesDisplay'
 import ChatDisplay from './ChatDisplay'
 
-const ChatContainer = () => {
+const ChatContainer = ({ user }) => {
     return <div className="chat-container">
-        <ChatHeader/>
+        <ChatHeader user={user}/>
         <div>
             <button className="option">Matches</button>
             <button className="option">Chat</button>
